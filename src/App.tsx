cd728e63bf73ec51047f9ad5b5b2a854960ebfda
ToKaKarTsivadze <tokaqarco2001@gmail.com>
2023-11-30 16:55:35 +0400

    added recipes and redesigned loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Search from "./components/search";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TechApi from "./components/APIs/recipesApi";
 import StoreApi from "./components/APIs/storeApi";
 import TodoApi from "./components/APIs/todoApi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
